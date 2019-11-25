53cf6fe3e99a5f4d274f90f6d2ce728fba7329fe
Mike-EEE <michael@dragonspark.network>
2019-11-25 09:30:01 -0500

    pwsh-friendly calls.

--- .build/Generate-Changelog.ps1
+++ .build/Generate-Changelog.ps1
@@ -12,5 +12,5 @@ Param([Parameter(Mandatory=$true)]
 {{/releases}}
 "@)
 Install-Module Poshstache -Force
-$response = Invoke-RestMethod https://api.github.com/repos/$Owner/$Repository/releases | Sort-Object published_at -Descending | ConvertTo-Json | ConvertFrom-Json
-ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject (@{ releases = $response.value } | ConvertTo-Json)
+$response = Invoke-RestMethod https://api.github.com/repos/$Owner/$Repository/releases | Sort-Object published_at -Descending
+ConvertTo-PoshstacheTemplate -InputString $Template -ParametersObject (@{ releases = $response } | ConvertTo-Json)
