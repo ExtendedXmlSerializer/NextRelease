f43f42ddbf14c84f5e8a9082779239bb5606bebe
Mike-EEE <michael@dragonspark.network>
2019-11-27 04:09:32 -0500

    Testing cache

--- .build/Deploy-Documentation.ps1
+++ .build/Deploy-Documentation.ps1
@@ -1,9 +1,10 @@
-<#
 $key = ("-----BEGIN RSA PRIVATE KEY-----`n" +
 		$env:DEPLOY_KEY.Replace(' ', "`n") +
 		"`n-----END RSA PRIVATE KEY-----`n")
 Set-Content "$Home\.ssh\id_rsa" $key
 git clone "git@github.com:ExtendedXmlSerializer/documentation.git" -b gh-pages .wwwroot -q
+
+<#
 Copy-Item .wwwroot/.git documentation/.wwwroot -recurse
 CD documentation/.wwwroot
 git config user.email $env:DEPLOY_EMAIL
